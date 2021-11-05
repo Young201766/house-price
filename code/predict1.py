b17e7d2e074decca80665dd0721bcaf009321481
Young201766 <67818283+Young201766@users.noreply.github.com>
2021-11-05 17:06:46 +0800

    Add files via upload

---
+++ code/predict1.py
@@ -0,0 +1,85 @@
+#决策树
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+train = pd.read_csv('data/train.csv')
+test=pd.read_csv('data/test.csv')
+train_y=train.SalePrice
+#选择数据集中的部分列
+predictor_cols=['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
+#直方图
+#y_train = train.pop('SalePrice')
+#y_train.hist()
+#y_train = np.log1p(y_train)
+# 相关图
+corrmat = train.corr()
+plt.subplots(figsize=(12,9))
+sns.heatmap(corrmat, vmax=0.9, square=True)
+plt.show()
+
+#缺失率
+df = pd.concat((train, test), axis=0)
+print(pd.isnull(df).sum())
+print(df.isnull().sum().sort_values(ascending=False).head(10))
+df_na = (df.isnull().sum() / len(df)) * 100
+df_na = df_na.drop(df_na[df_na == 0].index).sort_values(ascending=False)
+missing_data = pd.DataFrame({'缺失率': df_na})
+print(missing_data.head(10))
+f, ax = plt.subplots(figsize=(15,12))
+plt.xticks(rotation='90')
+sns.barplot(x=df_na.index, y=df_na)
+plt.xlabel('Features', fontsize=15)
+plt.ylabel('Percent of missing values', fontsize=15)
+plt.title('Percent missing data by feature', fontsize=15)
+plt.show()
+#影响价格的前十个变量
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+k  = 10 # 关系矩阵中将显示10个特征
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index ##显示和saleprice相近的十个关系变量矩阵
+cm = np.corrcoef(train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+train_X=train[predictor_cols]
+my_model=DecisionTreeRegressor()
+my_model.fit(train_X,train_y)
+
+DecisionTreeRegressor(criterion='mse', max_depth=None, max_features=None,
+           max_leaf_nodes=None, min_impurity_decrease=0.0,
+           min_samples_leaf=1,
+           min_samples_split=2, min_weight_fraction_leaf=0.0,
+           random_state=None, splitter='best')
+
+text_X=test[predictor_cols]
+predicted_prices=my_model.predict(text_X)
+print(predicted_prices)
+#my_submission=pd.DataFrame({'Id':test.Id,'SalePrice':predicted_prices})
+
+#保存预测结果到新的csv文件中
+#my_submission.to_csv('data/submission1.csv',index=False)
+#使用MAE度量模型的误差
+from sklearn.metrics import mean_absolute_error
+#决策树模型的参数max_leaf_nodes会影响模型表现，自定义函数探究该变量
+def get_mae(train_X, val_X,train_y,val_y):
+    model = DecisionTreeRegressor()
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+#在实际应用中，我们应该将原始数据集划分为训练集和测试集
+#训练集用来训练模型，测试集用来测试模型表现
+from sklearn.model_selection import train_test_split
+# split data into training and validation data, for both predictors and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(train_X, train_y,random_state = 0)
+
+
+my_mae = get_mae(train_X, val_X, train_y, val_y)
+print("mae:  %d" %(my_mae))
